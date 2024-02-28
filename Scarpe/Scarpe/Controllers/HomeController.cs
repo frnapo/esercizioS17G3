50936e2ddb237ae4cf3b81fd9b3ddc9307573965
Francesco <napo.fra96@gmail.com>
2024-02-28 16:08:25 +0100

    esercizio

---
+++ Scarpe/Scarpe/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using Scarpe.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace Scarpe.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Scarpe"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            List<Prodotti> prodotti = new List<Prodotti>();
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Articoli WHERE Visibile = 1";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Prodotti prodotto = new Prodotti();
+                    prodotto.ID = Convert.ToInt32(reader["idArticolo"]);
+                    prodotto.Nome = reader["Nome"].ToString();
+                    prodotto.Prezzo = Convert.ToDecimal(reader["Prezzo"]);
+                    prodotto.Immagine1 = reader["imgPath"].ToString();
+                    prodotti.Add(prodotto);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: ");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(prodotti);
+        }
+
+        public ActionResult Dettagli(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Scarpe"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            Prodotti prodotto = new Prodotti();
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Articoli WHERE idArticolo = " + id;
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    prodotto.ID = Convert.ToInt32(reader["idArticolo"]);
+                    prodotto.Nome = reader["Nome"].ToString();
+                    prodotto.Descrizione = reader["Descrizione"].ToString();
+                    prodotto.Prezzo = Convert.ToDecimal(reader["Prezzo"]);
+                    prodotto.Immagine1 = reader["imgPath"].ToString();
+                    prodotto.Immagine2 = reader["imgAlt1"].ToString();
+                    prodotto.Immagine3 = reader["imgAlt2"].ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: ");
+                Response.Write(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View(prodotto);
+        }
+
+        public ActionResult DeleteProd(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Scarpe"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+                string query = "UPDATE Articoli SET Visibile = 0 WHERE idArticolo = " + id;
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Scarpe/Scarpe/Models/Prodotti.cs
@@ -0,0 +1,15 @@
+﻿namespace Scarpe.Models
+{
+    public class Prodotti
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public string Immagine1 { get; set; }
+
+        public string Immagine2 { get; set; }
+
+        public string Immagine3 { get; set; }
+    }
+}
